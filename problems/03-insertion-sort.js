510c970fca18da2cd7d9b0c7e046dbffa14d586e
KevinKaras <kevinkaras2@gmail.com>
2021-01-26 15:26:04 -0600

    insertion sorting done

--- problems/03-insertion-sort.js
+++ problems/03-insertion-sort.js
@@ -1,8 +1,36 @@
 // Implement Insertion Sort
 
 function insertionSort(list) {
-  // your code here
+  for(let i = 1; i < list.length; i++){
+    let currentEl = list[i];
+    for(var j = i-1; j >= 0 && list[j] > currentEl; j--){
+      list[j+1] = list[j];
+    }
+    list[j+1] = currentEl;
+  }
+  return list;
 }
+
+let testArray = [4, 2, 5, 6, 9, 1]
+
+console.log(insertionSort(testArray));
+
+
+// psuedo
+// mark first element as sorted
+
+// for each unsorted element X
+
+//   'extract' the element X
+
+//   for j = lastSortedIndex down to 0
+
+//     if current element j > X
+
+//       move sorted element to the right by 1
+
+//     break loop and insert X here
+
 
 module.exports = {
   insertionSort
